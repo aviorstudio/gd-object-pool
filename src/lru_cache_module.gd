36ea156acee46c1fbf556c1250b839720e9806cc
Nico Zessoules <nicozessoules@gmail.com>
2026-03-04 21:56:36 -0700

    optimize lru promotion path

--- src/lru_cache_module.gd
+++ src/lru_cache_module.gd
@@ -7,8 +7,9 @@ static func create_cache(max_size: int = 50) -> LruCache:
 
 class LruCache extends RefCounted:
 	var _cache: Dictionary = {}
-	var _order: Array[String] = []
+	var _timestamps: Dictionary[String, int] = {}
 	var _max_size: int = 50
+	var _clock: int = 0
 
 	func _init(max_size: int = 50) -> void:
 		_max_size = max(1, max_size)
@@ -16,18 +17,33 @@ class LruCache extends RefCounted:
 	func get_value(key: String) -> Variant:
 		if not _cache.has(key):
 			return null
-		_order.erase(key)
-		_order.append(key)
+		_clock += 1
+		_timestamps[key] = _clock
 		return _cache[key]
 
 	func set_value(key: String, value: Variant) -> void:
 		if _cache.has(key):
-			_order.erase(key)
-		elif _order.size() >= _max_size:
-			var oldest: String = _order.pop_front()
-			_cache.erase(oldest)
+			_clock += 1
+			_timestamps[key] = _clock
+			_cache[key] = value
+			return
+		if _cache.size() >= _max_size:
+			_evict_oldest()
+		_clock += 1
 		_cache[key] = value
-		_order.append(key)
+		_timestamps[key] = _clock
+
+	func _evict_oldest() -> void:
+		var oldest_key: String = ""
+		var oldest_time: int = _clock + 1
+		for key in _timestamps:
+			var timestamp: int = int(_timestamps[key])
+			if timestamp < oldest_time:
+				oldest_time = timestamp
+				oldest_key = key
+		if oldest_key != "":
+			_cache.erase(oldest_key)
+			_timestamps.erase(oldest_key)
 
 	func has_key(key: String) -> bool:
 		return _cache.has(key)
@@ -37,4 +53,5 @@ class LruCache extends RefCounted:
 
 	func clear() -> void:
 		_cache.clear()
-		_order.clear()
+		_timestamps.clear()
+		_clock = 0
